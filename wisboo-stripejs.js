7e80cbcbeb1eeb9046b37261172c148a2e978376
Valentina Maldonado <c.valentinamm@gmail.com>
2019-04-17 14:10:01 -0400

    return a promise

--- wisboo-stripejs.js
+++ wisboo-stripejs.js
@@ -1,30 +1,49 @@
+function StripeProvider ($window, $document, $q) {
+  let stripePromise;
+
+  const loadStripeScript = () => {
+    if (!!this.stripe) {
+      return $q.resolve(this.stripe);
+    }
+
+    if (!$window.$StripeLoading) {
+      $window.$StripeLoading = true;
+      const deferred = $q.defer();
+      const script = $document[0].createElement('script');
+      script.async = true;
+      script.src = 'https://js.stripe.com/v3';
+      script.onload = () => {
+        this.stripe = $window.Stripe;
+        $window.$StripeLoading = false;
+        deferred.resolve(this.stripe);
+      };
+      script.onerror = () => {
+        $window.$StripeLoading = false;
+      };
+      $document[0].body.appendChild(script);
+      return stripePromise = deferred.promise;
+    }
+    return $q.when(stripePromise);
+  };
+
+  this.apiKey = null;
+
+  this.setApiKey = (apiKey) => {
+    this.apiKey = apiKey;
+  };
+
+  this.$get = () => {
+    return loadStripeScript().then(Stripe => {
+      return Stripe(this.key);
+    }, () => {
+      return {};
+    });
+  };
+}
+StripeProvider.$inject = ['$window', '$document', '$q'];
+
 const wisbooStripe = angular.module('wisboo.stripejs-wrapper', [])
-  .provider('stripe', function () {
-    this.loadStripeScript = () => {
-      if (!this.stripe) {
-        const script = document.createElement('script');
-        script.async = true;
-        script.src = 'https://js.stripe.com/v3';
-        script.onload = () => {
-          this.stripe = window.Stripe;
-        };
-        document.body.appendChild(script);
-      }
-    };
-
-    this.apiKey = null;
-
-    this.setApiKey = (apiKey) => {
-      this.apiKey = apiKey;
-    };
-
-    this.$get = () => {
-      if (this.stripe) {
-        return this.stripe(this.apiKey);
-      }
-      return Stripe(this.apiKey);
-    };
-  })
+  .provider('stripe', StripeProvider)
   .component('stripeCreditCardInput', {
     bindings: {
       instance: '<'

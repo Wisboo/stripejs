f918977b47efa8ceac7667a56e4e58f822a3969e
Valentina Maldonado <c.valentinamm@gmail.com>
2020-03-28 18:13:44 -0300

    use stripe official library to load script

--- wisboo-stripejs.js
+++ wisboo-stripejs.js
@@ -1,60 +1,7 @@
-function StripeProvider () {
-  let stripePromise;
-  this.apiKey = null;
+import { loadStripe } from '@stripe/stripe-js';
 
-  this.setApiKey = (apiKey) => {
-    this.apiKey = apiKey;
-  };
-
-  const loadStripeScript = ($q) => {
-    if (!!window.Stripe) {
-      return $q.resolve(window.Stripe);
-    }
-
-    if (!window.$StripeLoading) {
-      window.$StripeLoading = true;
-      const deferred = $q.defer();
-      const script = document.createElement('script');
-      script.async = true;
-      script.src = 'https://js.stripe.com/v3';
-      script.onload = () => {
-        window.$StripeLoading = false;
-        deferred.resolve(window.Stripe);
-      };
-      script.onerror = () => {
-        delete window.$StripeLoading;
-        deferred.reject();
-      };
-      document.body.appendChild(script);
-      return stripePromise = deferred.promise;
-    }
-    return $q.when(stripePromise);
-  };
-
-  this.$get = ['$q', function ($q) {
-    return {
-      instance: (options) => {
-        return loadStripeScript($q).then((Stripe) => Stripe(this.apiKey, options));
-      },
-      createCard: (options) => {
-        return loadStripeScript($q).then(Stripe => {
-          return $q.resolve(Stripe(this.apiKey).elements().create('card', options));
-        }, function () {
-          return $q.reject();
-        });
-      },
-      createToken: (cardElement, options) => {
-        return loadStripeScript($q).then(Stripe => {
-          return Stripe(this.apiKey).createToken(cardElement, options);
-        }, () => $q.reject());
-      },
-      redirectToCheckout: (accountId, sessionId) => {
-        return loadStripeScript($q).then(Stripe => {
-          return Stripe(this.apiKey, { stripeAccount: accountId }).redirectToCheckout({ sessionId });
-        }, () => $q.reject());
-      }
-    };
-  }];
+function StripeService () {
+  this.loadStripe = loadStripe;
 }
 
 const stripeCreditCardComponent = {
@@ -73,6 +20,6 @@ const stripeCreditCardComponent = {
 };
 
 const wisbooStripe = angular.module('wisboo.stripejs-wrapper', [])
-                            .provider('stripe', StripeProvider)
+                            .service('Stripe', StripeService)
                             .component('stripeCreditCardInput', stripeCreditCardComponent);
 export { wisbooStripe, stripeCreditCardComponent };
